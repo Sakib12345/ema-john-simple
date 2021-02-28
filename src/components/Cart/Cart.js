1f6b967f95cf340e7781ce6d2b876f883914627b
Sakib12345 <sakibalislam12345@gmail.com>
2021-02-28 10:08:19 +0600

    changed shipping cost

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,16 +8,17 @@ const Cart = (props) => {
     total + prd.price, 0);
 
     let shipping = 0;
-    if(total > 15){
-        shipping = 4.99;
-    }
-    else if(total > 0){
+    
+    if(total > 0){
         shipping = 12.99;
     }
     else if(total > 35){
         shipping = 0
     }
-
+    else if(total > 15){
+        shipping = 4.99;
+    }
+    
     const tax = (total / 10).toFixed(2);
     const grandTotal = total + shipping + Number(tax);
 
